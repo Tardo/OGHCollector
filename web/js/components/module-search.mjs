1425f5e9badde13b1ef044c50890c0422c328041
Alexandre D. Díaz <dev@redneboa.es>
2026-01-04 18:02:37 +0100

    imp: module page and module-search component

--- web/js/components/module-search.mjs
+++ web/js/components/module-search.mjs
@@ -32,13 +32,18 @@ class ModuleSearch extends Component {
     this.#el_search_results = this.queryId('results');
   }
 
+  onStart() {
+    super.onStart();
+    this.#el_search_results.style.display = 'none';
+  }
+
   onInputModuleSearch(ev) {
     const cur_active_item = this.#active_index;
     this.#active_index = -1;
     this.#updateActiveItem(cur_active_item);
     this.#last_query = ev.target.value.replaceAll(' ', '_');
     if (this.#last_query === '') {
-      this.#el_search_results.replaceChildren();
+      this.#fillResults();
     } else {
       this.#filterResults(this.#last_query).then(filter_info => {
         if (filter_info[0] === this.#last_query) {
@@ -65,10 +70,12 @@ class ModuleSearch extends Component {
   }
 
   #selectNextItem() {
+    const len_results = this.#el_search_results.children.length;
+    const max = Math.min(len_results, MAX_RESULTS);
     const cur_active_item = this.#active_index;
     this.#active_index += 1;
-    if (this.#active_index === MAX_RESULTS) {
-      this.#active_index = MAX_RESULTS - 1;
+    if (this.#active_index === max) {
+      this.#active_index = max - 1;
     }
     this.#updateActiveItem(cur_active_item);
   }
@@ -83,7 +90,7 @@ class ModuleSearch extends Component {
   }
 
   #updateActiveItem(last_active_index) {
-    if (last_active_index !== -1) {
+    if (typeof last_active_index !== 'undefined' && last_active_index !== -1) {
       const el_li = this.#el_search_results.children[last_active_index];
       el_li.classList.remove('active');
     }
@@ -91,7 +98,6 @@ class ModuleSearch extends Component {
       const el_li = this.#el_search_results.children[this.#active_index];
       el_li.classList.add('active');
       el_li.scrollIntoView({
-        behavior: 'smooth',
         block: 'center',
         inline: 'nearest',
       });
@@ -107,8 +113,16 @@ class ModuleSearch extends Component {
   }
 
   #fillResults(results) {
+    if (typeof results === 'undefined') {
+      this.#el_search_results.style.display = 'none';
+      return;
+    }
+
     this.#el_search_results.replaceChildren();
-    let result_items = results.map(module => {
+    const count_items = results.length;
+    const results_to_render =
+      count_items > MAX_RESULTS ? results.slice(0, MAX_RESULTS) : results;
+    let result_items = results_to_render.map(module => {
       const item_container = document.createElement('li');
       const item = document.createElement('a');
       item.classList.add('item');
@@ -117,8 +131,7 @@ class ModuleSearch extends Component {
       item_container.appendChild(item);
       return item_container;
     });
-    const count_items = result_items.length;
-    if (count_items > MAX_RESULTS) {
+    if (results_to_render.length !== count_items) {
       result_items = result_items.slice(0, MAX_RESULTS);
       const item_container = document.createElement('li');
       item_container.style.textAlign = 'center';
@@ -128,7 +141,16 @@ class ModuleSearch extends Component {
       item_container.textContent = `${count_items - MAX_RESULTS} hidden...`;
       result_items.push(item_container);
     }
-    this.#el_search_results.append(...result_items);
+    if (count_items === 0) {
+      this.#el_search_results.style.display = 'none';
+    } else {
+      this.#el_search_results.append(...result_items);
+      if (this.#active_index === -1) {
+        this.#active_index = 0;
+        this.#updateActiveItem();
+      }
+      this.#el_search_results.style.display = '';
+    }
   }
 }
 
